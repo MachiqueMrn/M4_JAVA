698e1ba1235174c938440ddcbf500c965fecc69f
Carlos Machique <91218185+MachiqueMrn@users.noreply.github.com>
2026-08-11 15:46:19 +0000

    S02

---
+++ S01/POO/Triangulo.java
@@ -0,0 +1,16 @@
+package S01.POO;
+
+public class Triangulo {
+    
+    double base;
+    double altura;
+
+    public Triangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double calcularArea() {
+        return base * altura / 2;
+    }
+}
